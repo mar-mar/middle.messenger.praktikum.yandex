0e84d7b02e18a41bb9c25fb825ec32943e44a314
Marina Mokrousova <rar-mar-rar@yandex.ru>
2023-02-15 09:23:18 +0300

    Добавляю пробелы, alt, по-другому пробрасываю name, добавляю open и порт

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+console.info(process.env.PORT);
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('dist'));
 app.use('/node_modules/@fontsource', express.static('node_modules/@fontsource'));
 app.use('/static', express.static('static'));
-
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
