00a3269b850997302a0d0d52db302dda7081c4e2
Marina Mokrousova <rar-mar-rar@yandex.ru>
2023-03-04 04:54:01 +0300

    начинаю добавлять popup

--- src/components/pane/index.ts
+++ src/components/pane/index.ts
@@ -1,8 +1,16 @@
-import { _Block } from '../../utils/_Block';
+import { Props, _Block } from '../../utils/_Block';
 import template from './index.hbs';
 import styles from './styles.module.pcss';
 
-export default class PaneLayout extends _Block {
+type PaneLayoutProps = {
+    secBackColor?: 0|1;
+    withBorder?: 0|1;
+    withMarginTop?: 0|1;
+    addeClassName?: string;
+} & Props;
+
+
+export default class PaneLayout extends _Block<PaneLayoutProps> {
 
     protected getCompileOptions() {
         return { template, styles };
