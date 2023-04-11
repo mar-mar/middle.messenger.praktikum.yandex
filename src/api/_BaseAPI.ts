4395c6d557622374a39b19ee087e0c3709f2ba4c
Marina Mokrousova <rar-mar-rar@yandex.ru>
2023-04-11 04:08:24 +0300

    test AuthAPI

--- src/api/_BaseAPI.ts
+++ src/api/_BaseAPI.ts
@@ -1,17 +1,9 @@
-import HTTPTransport, { CONTENT_TYPE } from "../utils/transport/HTTPTransport";
+import HTTPTransport from "../utils/transport/HTTPTransport";
 
 export default abstract class BaseAPI {
     protected http: HTTPTransport;
 
-    protected constructor(groupPath: string, contentType: CONTENT_TYPE = CONTENT_TYPE.JSON) {
-        this.http = new HTTPTransport({ groupPath, contentType });
+    protected constructor(http: HTTPTransport) {
+        this.http = http;
     }
-
-    public create?(data: unknown): Promise<unknown>;
-
-    public read?(identifier?: string | number): Promise<unknown>;
-
-    public update?(identifier: string | number, data: unknown): Promise<unknown>;
-
-    public delete?(identifier: string | number): Promise<unknown>;
 }
